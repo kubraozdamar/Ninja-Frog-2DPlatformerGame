427c7aebb7efb7ffab6cb79cb1488e1bf557b6a9
kubraozdamar <32kubraozdamar32@gmail.com>
2023-02-22 18:58:11 +0300

    Player Movement and Animations.

---
+++ PlatformerGame2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private BoxCollider2D collider;
+    [SerializeField] LayerMask jumpableGround;
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpSpeed;
+    private float dirX;
+
+    private enum MovementState
+    {
+        idle,
+        running,
+        jump,
+        falling
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+
+        Movement();
+        Jump();
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        MovementState state;
+
+        if (dirX > 0)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if (dirX < 0)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+            anim.SetBool("running", false);
+        }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jump;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+}
